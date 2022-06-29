f544f14753bf5a10593274445b30bb6833c54ee0
bhaskaralakshmi07 <97425746+bhaskaralakshmi07@users.noreply.github.com>
2022-06-29 06:18:45 +0530

    def count_special_character(string):     special_char= 0
        for i in range(0, len(string)):  
          
            ch = string[i]
    
            if (string[i].isalpha()):  
                continue
            elif (string[i].isdigit()):
                continue
                
            else: 
                special_char += 1
                
                
        if special_char >= 1:    
            print("{}".format(special_char))  
        else:
            print("There are no Special Characters in this String.")
      
    # Driver Code
    if __name__ == '__main__' : 
        string = input()
        count_special_character(string)

--- count_spaces_in_a_string.py
+++ count_spaces_in_a_string.py
@@ -1,10 +1,25 @@
-def check_space(string):
-    count = 0
-    for i in string:
-        if i == " ":
-            count += 1
-         
-    return count
-string =input()
-print(check_space(string))
-    
+def count_special_character(string): 
+    special_char= 0
+   
+    for i in range(0, len(string)):  
+      
+        ch = string[i]
+
+        if (string[i].isalpha()):  
+            continue
+        elif (string[i].isdigit()):
+            continue
+            
+        else: 
+            special_char += 1
+            
+            
+    if special_char >= 1:    
+        print("{}".format(special_char))  
+    else:
+        print("There are no Special Characters in this String.")
+  
+# Driver Code
+if __name__ == '__main__' : 
+    string = input()
+    count_special_character(string)
