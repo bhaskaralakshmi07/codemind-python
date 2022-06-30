b83f4c9688f11d395149f1259245db583092c177
bhaskaralakshmi07 <97425746+bhaskaralakshmi07@users.noreply.github.com>
2022-06-30 05:50:23 +0530

    def prime(n):     c=0     for i in range(1,n+1):         if n%i==0:             c=c+1     if c==2:         return True     else:         return False def avg(x):     y=[]     for k in x:         if prime(k):             y.append(k)     s=sum(y)     g=s/len(y)     print(format(g, '.2f')) n=int(input()) x=list(map(int,input().split())) avg(x)

---
+++ Average_of_primes.py
@@ -0,0 +1,20 @@
+def prime(n):
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c=c+1
+    if c==2:
+        return True
+    else:
+        return False
+def avg(x):
+    y=[]
+    for k in x:
+        if prime(k):
+            y.append(k)
+    s=sum(y)
+    g=s/len(y)
+    print(format(g, '.2f'))
+n=int(input())
+x=list(map(int,input().split()))
+avg(x)
