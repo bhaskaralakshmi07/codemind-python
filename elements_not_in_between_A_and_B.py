55e652bcd64f389d0cacac8c54b0fa029798e926
bhaskaralakshmi07 <97425746+bhaskaralakshmi07@users.noreply.github.com>
2022-07-01 06:34:32 +0530

    def sumofelement(x):     y=[]     for i in x:         if i<n or i>m:             y.append(i)     if len(y)!=0:         print(*y)     else:         print("-1") n=int(input()) x=list(map(int,input().split())) n,m=map(int,input().split()) sumofelement(x)

---
+++ elements_not_in_between_A_and_B.py
@@ -0,0 +1,13 @@
+def sumofelement(x):
+    y=[]
+    for i in x:
+        if i<n or i>m:
+            y.append(i)
+    if len(y)!=0:
+        print(*y)
+    else:
+        print("-1")
+n=int(input())
+x=list(map(int,input().split()))
+n,m=map(int,input().split())
+sumofelement(x)
