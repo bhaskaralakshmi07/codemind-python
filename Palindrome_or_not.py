0082c6693ef16ae3274ecf57d5aa1af1d248ad8e
bhaskaralakshmi07 <97425746+bhaskaralakshmi07@users.noreply.github.com>
2022-10-16 06:43:34 +0530

    n = input() #print(n) i = 0 j = len(n)-1 count = 0 n = n.lower() #print(n) if len(n)%2!=0:     while i<=j:         if n[i]==n[j]:             count += 1         i += 1         j -= 1     if count==len(n)//2+1:         print(True)     else:         print(False) else:     while i<j:         if n[i]==n[j]:             count += 1         i += 1         j -= 1     if count==len(n)//2:         print(True)     else:         print(False)

---
+++ Palindrome_or_not.py
@@ -0,0 +1,27 @@
+n = input()
+#print(n)
+i = 0
+j = len(n)-1
+count = 0
+n = n.lower()
+#print(n)
+if len(n)%2!=0:
+    while i<=j:
+        if n[i]==n[j]:
+            count += 1
+        i += 1
+        j -= 1
+    if count==len(n)//2+1:
+        print(True)
+    else:
+        print(False)
+else:
+    while i<j:
+        if n[i]==n[j]:
+            count += 1
+        i += 1
+        j -= 1
+    if count==len(n)//2:
+        print(True)
+    else:
+        print(False)
